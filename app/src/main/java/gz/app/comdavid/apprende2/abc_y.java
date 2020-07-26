2ba018e93d81fc47d8fadc5f3ffa7de0f1e1612b
malu1985 <56706773+malu1985@users.noreply.github.com>
2020-07-25 19:33:12 -0500

    comentarios

--- app/src/main/java/gz/app/comdavid/apprende2/abc_y.java
+++ app/src/main/java/gz/app/comdavid/apprende2/abc_y.java
@@ -1,5 +1,6 @@
+//Paquete
 package gz.app.comdavid.apprende2;
-
+//Librerias
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
@@ -9,72 +10,78 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 public class abc_y extends AppCompatActivity {
+    //Declaramos las variables
     MediaPlayer mp;
-
+    //Metodo onCreate
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //LLamar la vista asociada
         setContentView(R.layout.activity_abc_y);
-
+        //Llamar el boton
         Button inicio= (Button) findViewById(R.id.casa_y);
+        //Metodo OnClick
         inicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Abrir una nueva actividad
                 Intent intent=new Intent(v.getContext(),MainActivity.class);
                 startActivityForResult(intent,0);
+                //Transiciones
                 overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                 mp.stop();
                 finish();
             }
         });
+        //Llamar el boton
         Button next= (Button) findViewById(R.id.siguiente_y);
+        //Metodo OnClick
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Abrir una nueva actividad
                 Intent intent=new Intent(v.getContext(),abc_z.class);
                 startActivityForResult(intent,0);
+                //Transiciones
                 overridePendingTransition(R.anim.left_in, R.anim.left_out);
                 mp.stop();
                 finish();
-
             }
         });
-
-
+        //Llamar el boton
         Button back= (Button) findViewById(R.id.anterioir_y);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //Abrir una nueva actividad
                 Intent intent=new Intent(v.getContext(),abc_x.class);
                 startActivityForResult(intent,0);
+                //Transiciones
                 overridePendingTransition(R.anim.right_in, R.anim.right_out);
                 mp.stop();
                 finish();
             }
         });
-
-
-
+        //Llamar el boton
         Button sonidoa = (Button)findViewById(R.id.sonido_y);
+        //Llamar el audio
         mp= MediaPlayer.create(this,R.raw.sonidoy);
         sonidoa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //Iniciar audio
                 mp.start();
-
             }
         });
-
+        //Llamar la imagen
         ImageView sonidoaa = (ImageView) findViewById(R.id.imageView2);
+        //Llamar el audio
         mp= MediaPlayer.create(this,R.raw.sonidoy);
         sonidoaa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //Iniciar audio
                 mp.start();
-
             }
         });
     }
--- app/src/main/java/gz/app/comdavid/apprende2/vocales_o.java
+++ app/src/main/java/gz/app/comdavid/apprende2/vocales_o.java
@@ -1,5 +1,6 @@
+//Paquete
 package gz.app.comdavid.apprende2;
-
+//Librerias
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
@@ -9,66 +10,78 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 public class vocales_o extends AppCompatActivity {
+    //Declaramos las variables
     MediaPlayer mp;
-
+    //Metodo onCreate
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //LLamar la vista asociada
         setContentView(R.layout.activity_vocales_o);
-
+        //Llamar el boton
         Button inicio= (Button) findViewById(R.id.casa_o);
+        //Metodo OnClick
         inicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Abrir una nueva actividad
                 Intent intent=new Intent(v.getContext(),MainActivity.class);
                 startActivityForResult(intent,0);
+                //Transiciones
                 overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                 mp.stop();
                 finish();
             }
         });
-
+        //Llamar el boton
         Button next= (Button) findViewById(R.id.siguiente_o);
+        //Metodo OnClick
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Abrir una nueva actividad
                 Intent intent=new Intent(v.getContext(),vocales_u.class);
                 startActivityForResult(intent,0);
+                //Transiciones
                 overridePendingTransition(R.anim.left_in, R.anim.left_out);
                 mp.stop();
                 finish();
             }
         });
-
+        //Llamar el boton
         Button back= (Button) findViewById(R.id.anterioir_o);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Abrir una nueva actividad
                 Intent intent=new Intent(v.getContext(),vocales_i.class);
                 startActivityForResult(intent,0);
+                //Transiciones
                 overridePendingTransition(R.anim.right_in, R.anim.right_out);
                 mp.stop();
                 finish();
             }
         });
-
+        //Llamar el boton
         Button sonidoa = (Button)findViewById(R.id.sonido_o);
+        //Llamar el audio
         mp= MediaPlayer.create(this,R.raw.sonidoo);
         sonidoa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //Iniciar audio
                 mp.start();
 
             }
         });
-
+        //Llamar la imagen
         ImageView sonidoaa = (ImageView) findViewById(R.id.imageView2);
+        //Llamar el audio
         mp= MediaPlayer.create(this,R.raw.sonidoo);
         sonidoaa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //Iniciar audio
                 mp.start();
 
             }
